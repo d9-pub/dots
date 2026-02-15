2f4e2a555bbe7b832f236b6d7cc5e731d5a493b4
jack <mariengabbint@gmail.com>
2026-02-15 23:31:49 +0700

    first commit

---
+++ nvim/lua/config/options.lua
@@ -0,0 +1,24 @@
+local set = vim.opt
+
+set.number = true
+
+set.tabstop = 4
+set.shiftwidth = 4
+set.autoindent = true
+set.expandtab = true
+
+set.ignorecase = true
+set.smartcase = true
+
+set.cursorline = true
+
+set.clipboard:append("unnamedplus")
+
+set.scrolloff = 8
+
+set.swapfile = false
+set.backup = false
+
+set.incsearch = true
+
+set.updatetime = 50
---
+++ packages.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+sudo pacman -Syu plasma-desktop xsettingsd kde-gtk-config plasma-pa plasma-nm sddm sddm-kcm kwalletmanager kwallet-pam nftables kitty dolphin ark firefox telegram-desktop wl-clipboard audacious obsidian ttf-firacode-nerd fastfetch imagemagick tmux steam virtualbox libreoffice-fresh-ru gwenview spectacle gimp ripgrep bat lsd fzf code btop ranger keepassxc tree nvme-cli smartmontools openssh samba qbittorrent openbsd-netcat prismlauncher nvtop blender clang rocm-hip-runtime obs-studio discord gparted lazygit alacritty fd eza
